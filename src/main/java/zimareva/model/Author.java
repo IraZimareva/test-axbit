a82554e8294a4b9b7dc7d985dab69a834d2dc590
Ira Zimareva <irazimaryova@gmail.com>
2022-12-09 12:56:17 +0400

    - refactor hard delete logic to author

--- src/main/java/zimareva/model/Author.java
+++ src/main/java/zimareva/model/Author.java
@@ -2,21 +2,17 @@ package zimareva.model;
 
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import zimareva.exception.AuthorWithBooksExistsException;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Entity
 @Table(name = "author")
 @SQLDelete(sql = "UPDATE author SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted = false")
 public class Author extends EntitySuperclass {
-    private static Logger logger = Logger.getLogger(EntitySuperclass.class.getName());
-
     private String lastname;
     private String firstname;
     private String middlename;
@@ -72,20 +68,6 @@ public class Author extends EntitySuperclass {
 
     public void addBook(Book book) {
         books.add(book);
-    }
-
-    @PreRemove
-    public void preRemove() {
-        logger.info("Pre remove. Attempt to remove author");
-        if (this.getBooks().isEmpty() || this.getBooks() == null) {
-            logger.info("Remove author without books");
-        } else {
-            logger.info("Author contains books! Dont remove!");
-            throw new AuthorWithBooksExistsException(
-                    this.getLastname() + this.getFirstname(),
-                    this.getId(),
-                    this.getBooks().size());
-        }
     }
 
     @Override
